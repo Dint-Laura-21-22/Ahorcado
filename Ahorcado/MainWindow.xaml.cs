42219c043ef439742fea611d41cef04c069d53fc
ArualFM <laura.fernandezmol@iesdoctorbalmis.com>
2021-10-29 22:48:03 +0200

    Acabo por fin el ahorcado

--- Ahorcado/MainWindow.xaml.cs
+++ Ahorcado/MainWindow.xaml.cs
@@ -1,125 +1,165 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Ahorcado
 {
-    /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        TextBlock palabra = new TextBlock();
-        String palabraAdivinar;
-        String rallitasPalabra;
-        char[] palabraSecretaArray;
+        List<Char> letras;
+        List<BitmapImage> ahorcado;
+        List<TextBlock> contenedorPalabra;
+        string palabraAdivinar;
+        int fallos = 0;
+        
         public MainWindow()
         {
 
             InitializeComponent();
-            // Creamos una lista para los botones //
-            List<Char> CaracteresTeclado = Enumerable.Range('A', 'Z' - 'A' + 1).Select(i => (Char)i).ToList<Char>();
-            CaracteresTeclado.Insert(14, 'Ñ');
-            
-           
-            // Añadimos los controles y rellenamos //
-            foreach (var letraCaracter in CaracteresTeclado)
+            CrearBotones();
+            ahorcado = new List<BitmapImage>();
+            EmpezarPartida();
+
+
+        }
+
+
+
+        public void CrearBotones()
+        {
+            letras = Enumerable.Range('A', 'Z' - 'A' + 1).Select(i => (Char)i).ToList<Char>();
+            letras.Insert(14, 'Ñ');
+
+            foreach (var letra in letras)
             {
-                Button letrasBoton = new Button();
-                letrasBoton.Tag = letraCaracter;
+                Button letras = new Button();
+                letras.Tag = letra;
                 TextBlock texto = new TextBlock();
-                texto.Text = letraCaracter.ToString();
+                texto.Text = letra.ToString();
                 Viewbox box = new Viewbox();
                 box.Child = texto;
-                letrasBoton.Content = box;
-                ContenedorLetrasUniformGrid.Children.Add(letrasBoton);
-                letrasBoton.Click += Button_Click;
+                letras.Content = box;
+                ContenedorLetrasUniformGrid.Children.Add(letras);
+                letras.Click += Button_Click;
+                
             }
-
-            // Creamos el contenedor de la palabra //
-            ScrollViewer scroll = new ScrollViewer();
-            scroll.Content = palabra;
-            scroll.HorizontalScrollBarVisibility = ScrollBarVisibility.Visible;
-            scroll.VerticalScrollBarVisibility = ScrollBarVisibility.Hidden;
-            PalabrasWrapPanel.Children.Add(scroll);
-            palabra.Style = (Style)Application.Current.Resources["PalabrasTexto"];
-            palabraAdivinar = PalabraSecretaRandom();
-            rallitasPalabra = RallitasPalabra(palabraAdivinar);
-            palabra.Text = rallitasPalabra;
-            palabra.Width = 800;
-            palabra.Height = 300;
-            palabraSecretaArray = palabraAdivinar.ToCharArray();
-
-
-
         }
 
-        private int estado = 4;
-        public BitmapImage GetStageImage()
+        private void EmpezarPartida()
         {
-            return new BitmapImage(new Uri(System.IO.Path.Combine(
-                Environment.CurrentDirectory,"../../assets/" + estado + ".jpg")));
-        }
-        public String PalabraSecretaRandom()
-        {
-            List<String> Palabras = new List<string>() { "PAMPLONA"/*,"PROGRAMADOR", "HIELO", "OSO", "JAVA", "GALLINA", "CONEJO","ÑORA"*/ };
-            Random rand = new Random();
-            return Palabras[rand.Next(0, Palabras.Count())];
+
+            CargarImagen();
+            PalabrasWrapPanel.Children.Clear();
+            Barras();
         }
 
-        private String RallitasPalabra(String cadena)
+        private void CargarImagen()
         {
-            StringBuilder PalabraRallitas = new StringBuilder();
-            for (int i = 0; i < cadena.Length; i++)
+            for (int i = 4; i < 11; i++)
             {
-                palabra.Text = PalabraRallitas.Append(" _ ").ToString();
+                var imagen = new BitmapImage(
+                new Uri(System.IO.Path.Combine(
+                    Environment.CurrentDirectory,
+                    "../../assets/" + i.ToString() + ".jpg")));
+                ahorcado.Add(imagen);
             }
-            return PalabraRallitas.ToString();
+        }
+
+        private string PalabraSecretaRandom()
+        {
+            Random gen = new Random();
+            List<String> listaPalabras = new List<string>() { "ÑORA", "ALMENDRA", "CABALLO", "CONEJO", "PERRO", "PROGRAMADOR", "JAVA", "OSO", "GALLINA","PAMPLONA" };
+            return listaPalabras[gen.Next(0,listaPalabras.Count)];
+        }
+
+        private void Barras()
+        {
+            fallos = 0;
+            this.palabraAdivinar = PalabraSecretaRandom();
+            ImagenAhorcadoImage.Source = ahorcado[0];
+            contenedorPalabra = new List<TextBlock>();
+
+            for (int i = 0; i < this.palabraAdivinar.Length; i++)
+            {
+                TextBlock contenedor = new TextBlock()
+                {
+                    Text = "_",
+                };
+                contenedor.Style = (Style)this.Resources["letrasTextBlock"];
+                PalabrasWrapPanel.Children.Add(contenedor);
+                contenedorPalabra.Add(contenedor);
+                contenedor.FontSize = 40;
+            }
+        }
+
+        public void Juego(string letra)
+        {
+            bool acierto = false;
+
+            for (int i = 0; i < this.palabraAdivinar.Length; i++)
+            {
+                if (palabraAdivinar[i].ToString().Equals(letra))
+                {
+                    acierto = true;
+                    contenedorPalabra[i].Text = letra;
+                }
+            }
+            if (acierto == false)
+            {
+                fallos++;
+                ImagenAhorcadoImage.Source = ahorcado[fallos];
+            }
+            if (fallos == 6)
+            {
+                PerderPartida();
+            }
+            int contador = 0;
+            for (int i = 0; i < palabraAdivinar.Length; i++)
+            {
+                if (contenedorPalabra[i].Text != "_")
+                    contador++;
+            }
+            if (contador == palabraAdivinar.Length)
+            {
+                GanarPartida();
+            }
 
         }
 
-        // Cambio de texto si la palabra es correcta //
+        public void GanarPartida()
+        {
+            MessageBox.Show("Biien la palabraAdivinar era " + palabraAdivinar);
+        }
+        public void PerderPartida()
+        {
+
+            ImagenAhorcadoImage.Source = ahorcado[6];
+
+            MessageBox.Show(" =( la palabraAdivinar era " + palabraAdivinar);
+        }
+
+        //EVENTOS
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Button boton = (Button)sender;
+
+            string letra = boton.Tag.ToString();
+
+            Juego(letra);
             boton.IsEnabled = false;
-            char letraPulsada = (char)boton.Tag;
-            Comprobar(letraPulsada);
-
         }
 
-        public void Comprobar(char letra)
+
+        private void ReiniciarButton_Click(object sender, RoutedEventArgs e)
         {
-         
-           
-            for (int i = 0; i < palabraAdivinar.Length; i++)
-            {
-                if (palabraSecretaArray[i].Equals(letra))
-                {
-                    palabra.Text= rallitasPalabra.Replace(rallitasPalabra[i], letra).ToString();
-                }
-                else
-                {
-                    MessageBox.Show("Letra incorrecta");
-                }
-            }
-             
+            ImagenAhorcadoImage.Source = ahorcado[6];
+            MessageBox.Show("Te has rendido \nLa palabraAdivinar era " + palabraAdivinar);
         }
 
-       
-
-        // Añadir al XAML//
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
 
@@ -128,38 +168,23 @@ namespace Ahorcado
                 if (b.Tag.ToString() == e.Key.ToString())
                 {
                     b.IsEnabled = false;
-                    Comprobar((char)b.Tag);
+                    Juego(b.Tag.ToString());
                 }
-            }
-        }
-
-
-
-        private void ReiniciarButton_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Te has rendido" + palabraAdivinar);
-            estado = 10;
-            palabraAdivinar = PalabraSecretaRandom();
-            ImagenAhorcadoImage.Source = GetStageImage();
-            foreach (Button b in ContenedorLetrasUniformGrid.Children)
-            {
-                b.IsEnabled = true;
-                
             }
 
         }
 
         private void NuevaPartidaButton_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Has iniciado una nueva partida");
+            EmpezarPartida();
+            
             foreach (Button b in ContenedorLetrasUniformGrid.Children)
             {
                 b.IsEnabled = true;
-                
             }
-            estado = 4;
-            ImagenAhorcadoImage.Source = GetStageImage();
-            palabraAdivinar = PalabraSecretaRandom();
+            MessageBox.Show("Nueva Partida");
         }
     }
+
 }
+
